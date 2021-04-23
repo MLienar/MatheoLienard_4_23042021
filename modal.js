0a20b0d3621d4b83cb88772c19ffa0c1aea43499
matheolienard <matheolienard@gmail.com>
2021-04-23 11:10:08 +0200

    html and css fixes

---
+++ modal.js
@@ -0,0 +1,30 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+// Close modal event
+closeBtn.addEventListener("click", closeModal);
+
+// Close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
